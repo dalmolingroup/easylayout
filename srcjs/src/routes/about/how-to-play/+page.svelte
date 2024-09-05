d6a750d886456511a9427ea9595f238a0d68fc81
Danilo Imparato <imparatodanilo@gmail.com>
2024-09-06 02:00:52 +0800

    feat: introduces svelte template

---
+++ srcjs/src/routes/about/how-to-play/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>How to play Sverdle</title>
+	<meta name="description" content="How to play Sverdle" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>How to play Sverdle</h1>
+
+	<p>Stuff.</p>
+</div>
