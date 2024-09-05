2930cc451daa238d8368ffa7045ce7d64752aee6
Danilo Imparato <imparatodanilo@gmail.com>
2024-09-06 05:44:24 +0800

    build: configs clean build paths for the js lib

--- srcjs/vite.config.js
+++ srcjs/vite.config.js
@@ -4,4 +4,15 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    outDir: "../inst/www",
+    target: "es2015",
+    rollupOptions: {
+      output:{
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "assets/[name][extname]",
+      }
+    },
+  },
 })
