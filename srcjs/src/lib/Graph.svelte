0d7c61499b55924f40ba7993fe6e3d234d4970ee
Danilo Imparato <imparatodanilo@gmail.com>
2024-09-09 01:38:10 +0800

    feat: introduces a vivagraph component

---
+++ srcjs/src/lib/Graph.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { onMount } from 'svelte';
+  import Viva from 'vivagraphjs';
+
+  let container;
+
+  onMount(() => {
+    const graph = Viva.Graph.graph();
+
+    graph.addNode('A');
+    graph.addNode('B');
+    graph.addLink('A', 'B');
+
+    const renderer = Viva.Graph.View.renderer(graph, {
+      container: container,
+      graphics: Viva.Graph.View.webglGraphics()
+    });
+
+    renderer.run();
+  });
+</script>
+
+<div bind:this={container} style="width: 600px; height: 400px;"></div>
+
+<style>
+  div {
+    border: 1px solid #ccc;
+  }
+</style>
