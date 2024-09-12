db114e74fad04755ffa3c92750764e5e5995ee77
Danilo Imparato <imparatodanilo@gmail.com>
2024-09-13 06:07:33 +0800

    build: bumps app dist

--- inst/www/shared/shiny.js
+++ inst/www/shared/shiny.js
@@ -1,2 +1,10 @@
 // mock
-Shiny = "Behold: I am Shiny.";
+Shiny = {
+    setInputValue: (input, value) => {
+        console.log(`Mock: setInputValue(${input}, ${value})`);
+    },
+    addCustomMessageHandler: (message, callback) => {
+        console.log(`Mock: addCustomMessageHandler(${message}, ${callback})`);
+        callback("whatever");
+    },
+};
