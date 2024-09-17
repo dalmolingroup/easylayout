2e75e88ae2835066d51d92df450d05c2d4a951d3
Danilo Imparato <imparatodanilo@gmail.com>
2024-09-18 07:50:28 +0800

    fix: wrong attribute for size in editor mode

--- srcjs/src/lib/graphJSON.dev.js
+++ srcjs/src/lib/graphJSON.dev.js
@@ -1,9 +1,9 @@
 const graphJSON = {
     nodes: [
-        { "id": "1", },
-        { "id": "2", },
-        { "id": "3", },
-        { "id": "4", },
+        { "id": "1", "size": 20, "color": "#ff0000" },
+        { "id": "2", "size": 40, "color": "#00ff00" },
+        { "id": "3", "size": 5 },
+        { "id": "4", "color": "#0000ff" },
     ],
     links: [
         { "from": "1", "to": "2" },
